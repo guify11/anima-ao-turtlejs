c9443ef6113684f4b3e456cfc9962e3fe2fec714
guify11 <guify.kmail@gmail.com>
2026-06-03 19:15:52 +0100

    adicionadas funçoes basicas

--- js-turtle-master/turtle.js
+++ js-turtle-master/turtle.js
@@ -1,1 +1,21 @@
 /// <reference path="./lib/Intellisense/js-turtle_hy.ts" />
+var localizasaoX = 0
+var localizasaoY = 0
+
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max-min +1)) + min;
+}
+
+function ir(x, y){
+    localizasaoY = y
+    localizasaoX = x
+    goto(x, y)
+}
+
+function limpar(){
+    ir(0,0)
+    clear()
+    angle(0)
+    
+}
+pendown()
